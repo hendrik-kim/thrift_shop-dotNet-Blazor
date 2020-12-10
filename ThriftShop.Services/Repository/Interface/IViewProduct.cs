7f7f33fb24b37808d0785b73af74619d560a526f
hank <hank@ncrsystems.ca>
2020-12-10 09:31:33 -0600

    add view product component

--- ThriftShop.Services/Repository/Interface/IViewProduct.cs
+++ ThriftShop.Services/Repository/Interface/IViewProduct.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ThriftShop.Data.Models;
 
 namespace ThriftShop.Services.Repository.IRepository
 {
     public interface IViewProduct
     {
+        Product Execute(int id);
     }
 }
